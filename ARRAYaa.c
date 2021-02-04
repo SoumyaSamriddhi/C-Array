b452970469ad26f7d8b982a4bdbbea09dbed2e4b
SoumyaSamriddhi <78063739+SoumyaSamriddhi@users.noreply.github.com>
2021-02-04 11:09:29 +0530

    Add files via upload

--- ARRAYaa.c
+++ ARRAYaa.c
@@ -3,7 +3,7 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n,arr[n];
+    int i,j,n,max_right,arr[n];
     printf("size of array\n");
     scanf("%d",&n);
 
@@ -12,7 +12,7 @@ int main()
         scanf("%d ",&arr[i]);
     }
     
-
+/*
     for(i=0;i<n;i++){
         for(j=i+1;j<n;j++){
             if(arr[i]>arr[j])
@@ -21,8 +21,15 @@ int main()
         if(j==n);
         printf("%d",arr[i]);
     }
-
-
+*/
+    max_right=arr[n-1];
+    for(i=n-2;i>=0;i--){
+        if(arr[i]>max_right)
+        {
+         max_right=arr[i];
+         printf("%d",max_right);
+        }
+    }
 
 
     return 0;
